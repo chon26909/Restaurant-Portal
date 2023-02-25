34d06a334aaa00c588f6867aa9acb6c80dd2fa15
chon26909 <59905927+chon26909@users.noreply.github.com>
2023-02-25 23:48:52 +0700

    style sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,68 @@
+import { FC } from "react";
+import { NavLink } from "react-router-dom";
+// import { useAppSelector } from '../hooks';
+// import Logo from './Logo';
+import styles from "../styles/sidebar.module.scss";
+// import { ISidebar } from "../types/nav";
+
+const Sidebar: FC = () => {
+  // const { sidebar } = useAppSelector((state) => state);
+
+  const menu_sidebar: any[] = [
+    {
+      name: "ภาพรวม",
+      path: "/dashboard",
+      icon: <i className="fa-solid fa-gauge" />,
+    },
+    {
+      name: "สินค้า",
+      path: "/products",
+      icon: <i className="fa-solid fa-box" />,
+    },
+    {
+      name: "ข้อมูลลูกค้า",
+      path: "/customers",
+      icon: <i className="fa-solid fa-users" />,
+    },
+    {
+      name: "คำสั่งซื้อ",
+      path: "/orders",
+      icon: <i className="fa-solid fa-box-open" />,
+    },
+    {
+      name: "ประวัติการซื้อ",
+      path: "/history",
+      icon: <i className="fa-solid fa-clock-rotate-left" />,
+    },
+    {
+      name: "ผู้ดูแล",
+      path: "/role",
+      icon: <i className="fa-solid fa-user" />,
+    },
+  ];
+
+  return (
+    <div
+      className={[styles.Sidebar, true ? styles.SidebarActive : ""].join(" ")}
+    >
+      <div className={styles.Menu}>
+        {menu_sidebar.map((item: any, index: any) => {
+          return (
+            <NavLink
+              to={item.path}
+              className={({ isActive }) => {
+                return [styles.Item, isActive ? styles.Active : ""].join(" ");
+              }}
+              key={index}
+            >
+              <div>{item.icon}</div>
+              <div>{item.name}</div>
+            </NavLink>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
