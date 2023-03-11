28070eccfea9f0f141c9121c6ba1884541d4fd3e
HiChon <59905927+chon26909@users.noreply.github.com>
2023-03-11 20:52:28 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
 import "./App.scss";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import router from "./router";
-import MainLayout from "./layouts/MainLayout";
+import Input from "./components/Input";
+// import router from "./router";
+// import MainLayout from "./layouts/MainLayout";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {router.public.map((route) => (
-          <Route
-            key={route.path}
-            path={route.path}
-            element={<route.element />}
-          />
-        ))}
 
-        <Route element={<MainLayout />}>
-          {router.private.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={<route.element />}
-            />
-          ))}
-        </Route>
+  return <div>
+    <Input type="text" label={""}/>
+    <Input type="password" label={""}/>
+  </div>
 
-        <Route index element={<Navigate to="dashboard" />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  // return (
+  //   <BrowserRouter>
+  //     <Routes>
+  //       {router.public.map((route) => (
+  //         <Route
+  //           key={route.path}
+  //           path={route.path}
+  //           element={<route.element />}
+  //         />
+  //       ))}
+
+  //       <Route element={<MainLayout />}>
+  //         {router.private.map((route) => (
+  //           <Route
+  //             key={route.path}
+  //             path={route.path}
+  //             element={<route.element />}
+  //           />
+  //         ))}
+  //       </Route>
+
+  //       <Route index element={<Navigate to="dashboard" />} />
+  //     </Routes>
+  //   </BrowserRouter>
+  // );
 }
 
 export default App;
