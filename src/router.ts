c78ec8b621e801ea30b072d7ebc376f348c41ddb
HiChon <59905927+chon26909@users.noreply.github.com>
2023-03-11 23:03:32 +0700

    create router

---
+++ src/router.ts
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import DashboardPage from "./pages/DashboardPage";
+import LoginPage from "./pages/LoginPage";
+
+interface IRoutesType {
+    [key: string]: { path: string; element: FC }[]
+}
+
+const router: IRoutesType = {
+    public: [
+        {
+            path: "/",
+            element: LoginPage
+        }
+    ],
+    private: [
+        {
+            path: "/dashboard",
+            element: DashboardPage
+        }
+    ]
+}
+
+export default router;
