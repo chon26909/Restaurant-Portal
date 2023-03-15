bb941f5fb442bde376c916c7b70e5e92c8d42d7e
HiChon <59905927+chon26909@users.noreply.github.com>
2023-03-15 23:56:16 +0700

    create login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
 import Input from '../components/Input'
 
 const LoginPage = () => {
   return (
-    <div>
-      <div>
-        <Input type='text' label='Email' />
-      </div>
-      <div>
-        <Input type='password' label='Password' />
-      </div>
-      
+    <div className='flex justify-center items-center h-screen'>
+
+      <form className='shadow-2xl p-16 rounded-md'>
+        <div className='text-center text-2xl mb-3'>เข้าสู่ระบบ</div>
+        <div>
+          <Input type='text' label='Email' />
+        </div>
+        <div>
+          <Input type='password' label='Password' />
+        </div>
+      </form>
+
+
     </div>
   )
 }
