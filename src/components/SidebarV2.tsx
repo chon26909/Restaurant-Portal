b1ab11b1ce2f77545a7736067e2d4c92c278be3f
HiChon <59905927+chon26909@users.noreply.github.com>
2023-03-23 23:18:36 +0700

    sidebar

--- src/components/SidebarV2.tsx
+++ src/components/SidebarV2.tsx
@@ -4,62 +4,171 @@ import { FiChevronDown } from 'react-icons/fi'
 interface Menu {
     name: string
     submenu?: Menu[]
+    collapsed?: boolean
 }
+
+const MENU_DATA = [
+    {
+        name: "จัดการลูกค้า",
+        submenu: [
+            {
+                name: "ลูกค้าใหม่"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            }
+        ]
+    },
+    {
+        name: "จัดการลูกค้า",
+        submenu: [
+            {
+                name: "ลูกค้าใหม่"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            },
+            {
+                name: "ลูกค้าปัจจุบัน"
+            }
+        ]
+    }
+].map((menu) => {
+    return {
+        ...menu,
+        collapsed: false
+    }
+})
 
 const SidebarV2 = () => {
 
-    const menus = [
-        {
-            name: "จัดการลูกค้า",
-            submenu: [
-                {
-                    name: "ลูกค้าใหม่"
-                },
-                {
-                    name: "ลูกค้าปัจจุบัน"
-                }
-            ]
-        },
-        {
-            name: "จัดการลูกค้า",
-            submenu: [
-                {
-                    name: "ลูกค้าใหม่"
-                },
-                {
-                    name: "ลูกค้าปัจจุบัน"
-                }
-            ]
-        }
-    ]
 
     const Menu: FC = () => {
-        return <div>
-            {menus.map((item: Menu) => {
-                return <div>
-                    <div className="p-5 active:bg-white active:opacity-10 select-none flex justify-between">
+
+        const [menus, setMenus] = useState(MENU_DATA)
+
+        const onCollapse = (index: number) => {
+
+            setMenus(menus => {
+                return menus.map((menu, i) => i === index ? { ...menu, collapsed: !menu.collapsed } : menu)
+            })
+        }
+
+        return <>
+            {menus.map((item: Menu, index) => {
+                return <div key={index}>
+                    <div className="p-5 active:bg-white active:opacity-10 select-none flex justify-between cursor-pointer" onClick={() => onCollapse(index)}>
                         <div>{item.name}</div>
                         <div>
                             <FiChevronDown size={25} />
                         </div>
                     </div>
-                    <SubMenu submenu={item.submenu} />
+                    <SubMenu submenu={item.submenu} collapsed={item.collapsed} />
                 </div>
             })}
-        </div>
+        </>
     }
 
-    const SubMenu: FC<{ submenu?: Menu[] }> = (props) => {
-        return <div>
-            {props.submenu?.map((item: any) => {
-                return <div className="p-3 pl-7">{item.name}</div>
+    const SubMenu: FC<{ submenu?: Menu[], collapsed?: boolean }> = (props) => {
+
+        const isCollapsed = (collapsed?: boolean) => collapsed ? 'opacity-0 max-h-0 overflow-y-hidden' : 'opacity-1 max-h-[300px] overflow-y-auto';
+
+        return <div className={['transition-all ease-linear duration-200', isCollapsed(props.collapsed)].join(" ")} >
+            {props.submenu?.map((item: any, index) => {
+                return <div key={index} className="p-3 pl-7 active:bg-white active:opacity-10">{item.name}</div>
             })}
         </div>
     }
 
-
     return (
-        <div className="bg-primary w-[250px]">
+        <div className="bg-primary w-[250px] overflow-scroll">
             <Menu />
         </div>
     )
